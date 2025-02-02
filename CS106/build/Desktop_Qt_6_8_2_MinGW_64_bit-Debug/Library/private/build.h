304dc2c57c483ba97baa50da00342880d0b3289c
zouzhenshan <zouzhenshan@163.com>
2025-02-02 13:56:25 +0800

    add assignment 0

---
+++ CS106/build/Desktop_Qt_6_8_2_MinGW_64_bit-Debug/Library/private/build.h
@@ -0,0 +1,9 @@
+#ifndef SPL_BUILD_H
+#define SPL_BUILD_H
+
+#define SPL_VERSION "2021.1"
+#define SPL_INSTALL_LOCATION ""
+#define SPL_BUILD_DATE "Sun Feb 2 11:13:26 2025"
+#define SPL_BUILD_USER ""
+
+#endif
